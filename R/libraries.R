dc31c5c458eb9f26d22c276ae15a48ae656faa95
Hans Jacob Westbye <39839150+hjwestbye@users.noreply.github.com>
2024-01-10 00:03:44 +0100

    Delete R directory

--- R/libraries.R
+++
@@ -1,3 +0,0 @@
-if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
-if(!require(tidymodels)) install.packages("tidymodels", repos = "http://cran.us.r-project.org")
-if(!require(ggthemes)) install.packages("ggthemes", repos = "http://cran.us.r-project.org")
